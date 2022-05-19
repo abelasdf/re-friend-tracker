c4cf2e3a66435d5a831c5f4d966689608fdd760e
abelasdf <abelpsn98@gmail.com>
2022-05-19 16:44:47 +0000

    Test

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Abels Friend Tracker Test",
+            "title": "Abels Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
